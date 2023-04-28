a354f1f72d7da1e68514dff3a20d32ff739d6fc6
AlexDyvinets <66733339+AlexDyvinets@users.noreply.github.com>
2023-04-28 13:43:29 +0300

    Add files via upload

---
+++ client.js
@@ -0,0 +1,59 @@
+const output = document.getElementById('output');
+const input = document.getElementById('input');
+const sendButton = document.getElementById('send');
+const connectButton = document.getElementById('connect');
+const disconnectButton = document.getElementById('disconnect');
+
+let socket = null;
+
+function showMessage(message) {
+  const p = document.createElement('p');
+  p.innerText = message;
+  output.appendChild(p);
+}
+
+sendButton.addEventListener('click', () => {
+  if (socket && socket.readyState === WebSocket.OPEN) {
+    const message = input.value.trim();
+    if (message.match(/^[a-zA-Z\s]+$/)) {
+      socket.send(message);
+    } else {
+      showMessage('Sorry, message is invalid');
+    }
+    input.value = '';
+  } else {
+    showMessage('You are not connected to the server');
+  }
+});
+
+connectButton.addEventListener('click', () => {
+  if (!socket || socket.readyState === WebSocket.CLOSED) {
+    socket = new WebSocket('ws://localhost:8080');
+    socket.addEventListener('open', () => {
+      showMessage('Connected to the server');
+    });
+    socket.addEventListener('message', (event) => {
+      const message = event.data;
+      showMessage(`Server says: ${message}`);
+    });
+    socket.addEventListener('close', () => {
+      showMessage('Disconnected from the server');
+    });
+  } else {
+    showMessage('You are already connected to the server');
+  }
+});
+
+disconnectButton.addEventListener('click', () => {
+  if (socket && socket.readyState !== WebSocket.CLOSED) {
+    socket.close();
+  } else {
+    showMessage('You are not connected to the server');
+  }
+});
+
+input.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    sendButton.click();
+  }
+});
---
+++ server.js
@@ -0,0 +1,41 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({ port: 8080 });
+
+let messageCount = 0;
+let messageTimer;
+
+server.on('connection', (socket) => {
+  console.log('Client connected');
+
+  socket.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+
+    if (typeof message !== 'string') {
+      message = message.toString();
+    }
+
+    if (message.match(/^[a-zA-Z\s]+$/)) {
+      messageCount++;
+      socket.send('Your message is valid.');
+    } else {
+      socket.send('Sorry, message is invalid');
+    }
+  });
+
+  socket.on('close', () => {
+    console.log('Client disconnected');
+  });
+
+  // Устанавливаем таймер для регулярной отправки сообщений с количеством сообщений
+  messageTimer = setInterval(() => {
+    socket.send(`Total messages sent: ${messageCount}`);
+  }, 15000);
+});
+
+server.on('close', () => {
+  console.log('Server shutting down');
+
+  // Очищаем таймер перед закрытием сервера
+  clearInterval(messageTimer);
+});
